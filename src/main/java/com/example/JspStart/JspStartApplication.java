fb1057f20314fd0c1205a794d26eeda37e26af84
gusdl1992 <tlgusdl92@gmail.com>
2024-05-29 01:16:02 +0900

    Spring Boot 3.x 버전 JSP 셋팅 연습

---
+++ src/main/java/com/example/JspStart/JspStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.JspStart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspStartApplication.class, args);
+	}
+
+}
